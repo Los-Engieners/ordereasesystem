2f8fe2ef4bb3f05c4bfca2ec8988252fdc1361ae
Mairap12 <mairanallelyperezcortez@gmail.com>
2024-07-24 09:40:38 -0600

    Primer cambio

---
+++ src/main/java/org/orderease/OrdereasesystemApplication.java
@@ -0,0 +1,13 @@
+package org.orderease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdereasesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdereasesystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/orderease/OrdereasesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.orderease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdereasesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
